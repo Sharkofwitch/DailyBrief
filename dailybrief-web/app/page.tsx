19b7d70bf94829d317dc8f73e8ec30cff1176aac
Jakob Szarkowicz <franzklecks@gmail.com>
2025-02-05 16:45:26 +0100

    Added web-interface

---
+++ dailybrief-web/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold">DailyBrief</h1>
+      <p className="text-lg mt-2">Die neuesten Schlagzeilen auf einen Blick.</p>
+    </main>
+  );
+}
